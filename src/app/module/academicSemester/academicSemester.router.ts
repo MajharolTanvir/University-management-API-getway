88f8c399d5b3ed35e407a6e518fad58b847d210b
MajharolTanvir <majharul5456@gmail.com>
2023-09-16 11:39:32 +0600

    Academic semester zod validation functionality connected successfully

--- src/app/module/academicSemester/academicSemester.router.ts
+++ src/app/module/academicSemester/academicSemester.router.ts
@@ -1,12 +1,18 @@
 import express from 'express';
 import { AcademicSemesterController } from './academicSemester.controller';
+import validateRequest from '../../middlewares/validateRequest';
+import { AcademicSemesterValidation } from './academicSemester.validation';
 
 const router = express.Router();
 
 router.get('/', AcademicSemesterController.getAllAcademicSemester);
 router.get('/:id', AcademicSemesterController.getSingleAcademicSemester);
-router.post('/', AcademicSemesterController.createAcademicSemester);
-router.patch('/:id', AcademicSemesterController.updateAcademicSemester);
+router.post('/', validateRequest(AcademicSemesterValidation.create), AcademicSemesterController.createAcademicSemester);
+router.patch(
+  '/:id',
+  validateRequest(AcademicSemesterValidation.update),
+  AcademicSemesterController.updateAcademicSemester
+);
 router.delete('/:id', AcademicSemesterController.deleteAcademicSemester);
 
 
